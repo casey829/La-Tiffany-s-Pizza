1e10e511bda933ba97acb47585db674a72aaf588
Casey Kimamo <caseymwangi31@gmail.com>
2021-03-08 23:50:02 +0300

    changes

--- script.js
+++ script.js
@@ -12,7 +12,9 @@ $(document).ready(function () {
         this.city = city;
         this.street = street;
     }
-    function OrderCost(sizeCost, toppingCost, crustCost, deliveryCost, totalCost) {
+    function OrderCost(sizeCost, toppingCost, crustCost, deliveryCost, totalCost) 
+    
+
         this.sizeCost = sizeCost;
         this.toppingCost = toppingCost;
         this.crustCost = crustCost;
@@ -34,14 +36,21 @@ $(document).ready(function () {
         };
         $("#addOrder").click(function () {
             $("#pizzaOrder").append('neworder md-12'+
-            ' <select class="form-control>'+
+            ' <select class="form-control">'+
             '<option value="1">Small</option>'+
             '<option value="2">Medium</option>'+
             ' <option value="3">Large</option>'+
             '</select>'+
             '<label for="select" class="label">Choose topping(s) @ksh.70 each</label>'+
             '  <select class="form-control" id="1">'+' <option value="2">Hawaiian Pizza</option>'+'<option value="3">Veggie Pizza</option>'+'<option value="4">Pepperoni Pizza</option>'+'<option value="5">Meat Pizza</option>'+'<option value="6">Buffalo Pizza</option>'
-            +'</select>'+'</div>'+'<label for="select" class="label">Choose topping(s) @ksh.70 each</label>'+''+''
+            +'</select>'+'</div>'+'<label for="select" class="label">Choose topping(s) @ksh.70 each</label>'+'<select class="form-control" id="1">'
+            +'<option value="1">None</option>'+'<option value="1">Pineapple</option>'+'<option value="2">Mushrooms</option>'
+            +' <option value="3">Onions</option>'+' <option value="4">Sausage</option>'+' <option value="5">Bacon</option>'
+            +'  <option value="6">Extra cheese</option>'+' </select>'+
+            '<div class="col-md-12" id="crust">'+'<label for="select" class="label">Type Of Crust</label>'
+            +'<select class="form-control" id="crust">'+' <option value="1">Crispy----ksh.200</option>'
+            +'<option value="2">Stuffed---ksh.180</option>'+' <option value="3">Gluten-free---ksh.150</option>'
+            +'</select>'+''
             );
 
         });
