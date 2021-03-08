6b694babc3baf8c3f421039b44ba825a13d6c812
Casey Kimamo <caseymwangi31@gmail.com>
2021-03-08 13:03:41 +0300

    order here

---
+++ script.js
@@ -0,0 +1,17 @@
+//business logic
+
+var price , crust_price, topping_price ;
+let total = 0;
+function Getpizza( name,size,crust,topping, total ){
+  this.name = name;
+  this.size = size;
+  this.crust = crust;
+  this.topping = topping;
+  this.total = total;
+}
+
+//user interface
+
+$(document).ready(function(){
+
+});
